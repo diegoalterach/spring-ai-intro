0dce998743a3f2510db92017352f5438266b2f74
diegoalterach <diego.alterach@gmail.com>
2025-05-26 16:01:26 -0300

    formateando los retornos

--- src/main/java/guru/springframework/springaiintro/services/AIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/AIServiceImpl.java
@@ -1,12 +1,18 @@
 package guru.springframework.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -24,6 +30,9 @@ public class AIServiceImpl implements AIService {
     private Resource getCapitalPrompt;
     @Value("classpath:templates/get-capital-prompt-with-info.st")
     private Resource getCapitalPromptWithInfo;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     public AIServiceImpl(ChatModel chatModel) {
         this.chatModel = chatModel;
@@ -49,13 +58,23 @@ public class AIServiceImpl implements AIService {
     }
 
     @Override
-    public Answer getCapital(GetCapitalRequest getCapitalRequest) {
+    public GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest) {
+        BeanOutputConverter<GetCapitalResponse> converter = new BeanOutputConverter<>(GetCapitalResponse.class);
+        String format = converter.getFormat();
+        System.out.println(format);
+
+
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
 
-        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry",getCapitalRequest.stateOrCountry()));
+        Prompt prompt = promptTemplate.create(Map.of(
+                "stateOrCountry",getCapitalRequest.stateOrCountry(),
+                "format",format));
 
         ChatResponse response = chatModel.call(prompt);
-        return new Answer(response.getResult().getOutput().getContent());
+
+        System.out.println(response.getResult().getOutput().getContent());
+
+        return converter.convert(response.getResult().getOutput().getContent());
     }
 
     @Override
